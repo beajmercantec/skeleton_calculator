6be1aa43aa21fadbd67c9e96e03ccad657faa561
beajmercantec <beaj@mercantec.dk>
2025-09-27 16:58:02 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Velkommen til Lommeregner!");
+
+            // TODO: Spørg brugeren om to tal
+            // TODO: Spørg brugeren om en operator (+, -, *, /)
+            // TODO: Kald metoder i Calculator-klassen til at udføre beregningen
+            // TODO: Udskriv resultatet
+        }
+    }
+}
