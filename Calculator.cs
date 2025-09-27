f4f54ef14eea582b5183b2c1c1372aeb60ac0f34
beajmercantec <beaj@mercantec.dk>
2025-09-27 16:58:32 +0200

    Create Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            // TODO: Returnér a + b
+            return 0;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            // TODO: Returnér a - b
+            return 0;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            // TODO: Returnér a * b
+            return 0;
+        }
+
+        public double Divide(double a, double b)
+        {
+            // TODO: Returnér a / b (husk at tjekke division med 0!)
+            return 0;
+        }
+    }
+}
